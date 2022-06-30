c476d44d388dd892c3b1fd4df05b1f1770958568
Lucas Costa Pauli <lucaspauli22@gmail.com>
2022-06-30 15:55:00 -0300

    design changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -65,7 +65,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <h1>Mi<span className="highlight">xT</span>ape</h1>
   <div className="App">
     <SearchBar onSearch={this.search} />
     <div className="App-playlist">
